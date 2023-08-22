6ca8c906a848cfc63bb5b56d73e1b333d059145c
Mike Tickle <mtickle@gmail.com>
2023-08-22 07:39:33 -0400

    Adding generic delcarations

--- main.go
+++ main.go
@@ -86,42 +86,75 @@ func main() {
 	//--- Start iterating through the records
 	for _, rec := range apiResult.Aircraft {
 
-		var sb strings.Builder
+		hex := rec.Hex
+		ttype := rec.Type
+		flight := rec.Flight
+		alt_baro := rec.AltBaro
+		alt_geom := rec.AltGeom
+		gs := rec.Gs
+		track := rec.Track
+		baro_rate := rec.BaroRate
+		squawk := rec.Squawk
+		emergency := rec.Emergency
+		category := rec.Category
+		lat := rec.Lat
+		lon := rec.Lon
+		nic := rec.Nic
+		rc := rec.Rc
+		seen_pos := rec.SeenPos
+		r_dst := rec.RDst
+		r_dir := rec.RDir
+		version := rec.Version
+		nic_baro := rec.NicBaro
+		nac_p := rec.NacP
+		nac_v := rec.NacV
+		sil := rec.Sil
+		sil_type := rec.SilType
+		gva := rec.Gva
+		sda := rec.Sda
+		alert := rec.Alert
+		spi := rec.Spi
+		mlat := rec.Mlat
+		tisb := rec.Tisb
+		messages := rec.Messages
+		seen := rec.Seen
+		rssi := rec.Rssi
 
+		var sb strings.Builder
 		sb.WriteString("{")
-		sb.WriteString("\"hex\": \"" + rec.Hex + "\", ")
-		sb.WriteString("\"ttype\": \"" + rec.Type + "\", ")
-		sb.WriteString("\"flight\": \"" + rec.Flight + "\", ")
-		sb.WriteString("\"alt_baro\": \"" + fmt.Sprintf("%d", rec.AltBaro) + "\", ")
-		sb.WriteString("\"alt_geom\": \"" + fmt.Sprintf("%d", rec.AltGeom) + "\", ")
-		sb.WriteString("\"gs\": \"" + fmt.Sprintf("%g", rec.Gs) + "\", ")
-		sb.WriteString("\"track\": \"" + fmt.Sprintf("%g", rec.Track) + "\", ")
-		sb.WriteString("\"baro_rate\": \"" + fmt.Sprintf("%d", rec.BaroRate) + "\", ")
-		sb.WriteString("\"squawk\": \"" + rec.Squawk + "\", ")
-		sb.WriteString("\"emergency\": \"" + rec.Emergency + "\", ")
-		sb.WriteString("\"category\": \"" + rec.Category + "\", ")
-		sb.WriteString("\"lat\": \"" + fmt.Sprintf("%g", rec.Lat) + "\", ")
-		sb.WriteString("\"lon\": \"" + fmt.Sprintf("%g", rec.Lon) + "\", ")
-		sb.WriteString("\"nic\": \"" + fmt.Sprintf("%d", rec.Nic) + "\", ")
-		sb.WriteString("\"rc\": \"" + fmt.Sprintf("%d", rec.Rc) + "\", ")
-		sb.WriteString("\"seen_pos\": \"" + fmt.Sprintf("%g", rec.SeenPos) + "\", ")
-		sb.WriteString("\"r_dst\": \"" + fmt.Sprintf("%g", rec.RDst) + "\", ")
-		sb.WriteString("\"r_dir\": \"" + fmt.Sprintf("%g", rec.RDir) + "\", ")
-		sb.WriteString("\"version\": \"" + fmt.Sprintf("%d", rec.Version) + "\", ")
-		sb.WriteString("\"nic_baro\": \"" + fmt.Sprintf("%d", rec.NicBaro) + "\", ")
-		sb.WriteString("\"nac_p\": \"" + fmt.Sprintf("%d", rec.NacP) + "\", ")
-		sb.WriteString("\"nac_v\": \"" + fmt.Sprintf("%d", rec.NacV) + "\", ")
-		sb.WriteString("\"sil\": \"" + fmt.Sprintf("%d", rec.Sil) + "\", ")
-		sb.WriteString("\"sil_type\": \"" + rec.SilType + "\", ")
-		sb.WriteString("\"gva\": \"" + fmt.Sprintf("%d", rec.Gva) + "\", ")
-		sb.WriteString("\"sda\": \"" + fmt.Sprintf("%d", rec.Sda) + "\", ")
-		sb.WriteString("\"alert\": \"" + fmt.Sprintf("%d", rec.Alert) + "\", ")
-		sb.WriteString("\"spi\": \"" + fmt.Sprintf("%d", rec.Spi) + "\", ")
-		sb.WriteString("\"mlat\": \"" + fmt.Sprintf("%g", rec.Mlat) + "\", ")
-		sb.WriteString("\"tisb\": \"" + fmt.Sprintf("%g", rec.Tisb) + "\", ")
-		sb.WriteString("\"messages\": \"" + fmt.Sprintf("%d", rec.Messages) + "\", ")
-		sb.WriteString("\"seen\": \"" + fmt.Sprintf("%g", rec.Seen) + "\", ")
-		sb.WriteString("\"rssi\": \"" + fmt.Sprintf("%g", rec.Rssi) + "\", ")
+		sb.WriteString("\"hex\": \"" + hex + "\", ")
+		sb.WriteString("\"ttype\": \"" + ttype + "\", ")
+		sb.WriteString("\"flight\": \"" + flight + "\", ")
+		sb.WriteString("\"alt_baro\": \"" + fmt.Sprintf("%d", alt_baro) + "\", ")
+		sb.WriteString("\"alt_geom\": \"" + fmt.Sprintf("%d", alt_geom) + "\", ")
+		sb.WriteString("\"gs\": \"" + fmt.Sprintf("%g", gs) + "\", ")
+		sb.WriteString("\"track\": \"" + fmt.Sprintf("%g", track) + "\", ")
+		sb.WriteString("\"baro_rate\": \"" + fmt.Sprintf("%d", baro_rate) + "\", ")
+		sb.WriteString("\"squawk\": \"" + squawk + "\", ")
+		sb.WriteString("\"emergency\": \"" + emergency + "\", ")
+		sb.WriteString("\"category\": \"" + category + "\", ")
+		sb.WriteString("\"lat\": \"" + fmt.Sprintf("%g", lat) + "\", ")
+		sb.WriteString("\"lon\": \"" + fmt.Sprintf("%g", lon) + "\", ")
+		sb.WriteString("\"nic\": \"" + fmt.Sprintf("%d", nic) + "\", ")
+		sb.WriteString("\"rc\": \"" + fmt.Sprintf("%d", rc) + "\", ")
+		sb.WriteString("\"seen_pos\": \"" + fmt.Sprintf("%g", seen_pos) + "\", ")
+		sb.WriteString("\"r_dst\": \"" + fmt.Sprintf("%g", r_dst) + "\", ")
+		sb.WriteString("\"r_dir\": \"" + fmt.Sprintf("%g", r_dir) + "\", ")
+		sb.WriteString("\"version\": \"" + fmt.Sprintf("%d", version) + "\", ")
+		sb.WriteString("\"nic_baro\": \"" + fmt.Sprintf("%d", nic_baro) + "\", ")
+		sb.WriteString("\"nac_p\": \"" + fmt.Sprintf("%d", nac_p) + "\", ")
+		sb.WriteString("\"nac_v\": \"" + fmt.Sprintf("%d", nac_v) + "\", ")
+		sb.WriteString("\"sil\": \"" + fmt.Sprintf("%d", sil) + "\", ")
+		sb.WriteString("\"sil_type\": \"" + sil_type + "\", ")
+		sb.WriteString("\"gva\": \"" + fmt.Sprintf("%d", gva) + "\", ")
+		sb.WriteString("\"sda\": \"" + fmt.Sprintf("%d", sda) + "\", ")
+		sb.WriteString("\"alert\": \"" + fmt.Sprintf("%d", alert) + "\", ")
+		sb.WriteString("\"spi\": \"" + fmt.Sprintf("%d", spi) + "\", ")
+		sb.WriteString("\"mlat\": \"" + fmt.Sprintf("%g", mlat) + "\", ")
+		sb.WriteString("\"tisb\": \"" + fmt.Sprintf("%g", tisb) + "\", ")
+		sb.WriteString("\"messages\": \"" + fmt.Sprintf("%d", messages) + "\", ")
+		sb.WriteString("\"seen\": \"" + fmt.Sprintf("%g", seen) + "\", ")
+		sb.WriteString("\"rssi\": \"" + fmt.Sprintf("%g", rssi) + "\", ")
 		sb.WriteString("}")
 
 		temp := sb.String()
