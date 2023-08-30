6f435e8b06567712a3345622bd6ba5583d548507
Mike Tickle <mtickle@gmail.com>
2023-08-30 10:16:38 -0400

    Updated to use local postgres

---
+++ SQL/create table.sql
@@ -0,0 +1,42 @@
+-- Table: public.flight
+--DROP TABLE IF EXISTS public.flight;
+CREATE TABLE IF NOT EXISTS public.flight (
+    id bigint NOT NULL,
+    hex character varying(6) COLLATE pg_catalog."default",
+    type character varying(12) COLLATE pg_catalog."default",
+    flight character varying(255) COLLATE pg_catalog."default",
+    alt_baro bigint,
+    alt_geom bigint,
+    gs character varying(255) COLLATE pg_catalog."default",
+    track character varying(255) COLLATE pg_catalog."default",
+    baro_rate character varying(255) COLLATE pg_catalog."default",
+    category character varying(255) COLLATE pg_catalog."default",
+    nav_qnh character varying(255) COLLATE pg_catalog."default",
+    nav_altitude_mcp character varying(255) COLLATE pg_catalog."default",
+    lat character varying(255) COLLATE pg_catalog."default",
+    lon character varying(255) COLLATE pg_catalog."default",
+    nic character varying(255) COLLATE pg_catalog."default",
+    rc character varying(255) COLLATE pg_catalog."default",
+    seen_pos character varying(255) COLLATE pg_catalog."default",
+    r_dst character varying(255) COLLATE pg_catalog."default",
+    r_dir character varying(255) COLLATE pg_catalog."default",
+    version character varying(255) COLLATE pg_catalog."default",
+    nic_baro character varying(255) COLLATE pg_catalog."default",
+    nac_p character varying(255) COLLATE pg_catalog."default",
+    nac_v character varying(255) COLLATE pg_catalog."default",
+    sil character varying(255) COLLATE pg_catalog."default",
+    sil_type character varying(255) COLLATE pg_catalog."default",
+    gva character varying(255) COLLATE pg_catalog."default",
+    sda character varying(255) COLLATE pg_catalog."default",
+    alert character varying(255) COLLATE pg_catalog."default",
+    spi character varying(255) COLLATE pg_catalog."default",
+    mlat character varying(255) COLLATE pg_catalog."default",
+    tisb character varying(255) COLLATE pg_catalog."default",
+    messages character varying(255) COLLATE pg_catalog."default",
+    seen character varying(255) COLLATE pg_catalog."default",
+    rssi character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT flight_pkey PRIMARY KEY (id)
+) TABLESPACE pg_default;
+
+ALTER TABLE
+    IF EXISTS public.flight OWNER to pi;
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,7 +10,17 @@ import (
 	"os"
 	"strings"
 
+	_ "github.com/lib/pq"
+
 	"github.com/joho/godotenv"
+)
+
+const (
+	host     = "192.168.86.2"
+	port     = 5432
+	user     = "pi"
+	password = "Boomer2025"
+	dbname   = "adsb"
 )
 
 type Response struct {
@@ -70,12 +80,12 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
-	fxApiKey := os.Getenv("FX_API_KEY")
+	//fxApiKey := os.Getenv("FX_API_KEY")
 
 	//-----------------------------------------------------------------------
 	//--- Set the data url
 	fxSourceUrl := os.Getenv("FX_SOURCE_URL")
-	fxApiUrl := os.Getenv("FX_API_URL")
+	//fxApiUrl := os.Getenv("FX_API_URL")
 	//-----------------------------------------------------------------------
 
 	//-----------------------------------------------------------------------
@@ -92,11 +102,26 @@ func main() {
 	//-----------------------------------------------------------------------
 
 	//-----------------------------------------------------------------------
+	//--- Make and open the database connection
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//--- Are we good?
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	//-----------------------------------------------------------------------
+
+	//-----------------------------------------------------------------------
 	//--- Start iterating through the records
 	for _, rec := range apiResult.Aircraft {
-
-		hex := strings.ToUpper(strings.TrimSpace(rec.Hex))
-		ttype := strings.TrimSpace(rec.Type)
+		hex_code := strings.ToUpper(strings.TrimSpace(rec.Hex))
+		type_code := strings.TrimSpace(rec.Type)
 		flight := strings.TrimSpace(rec.Flight)
 		alt_baro := rec.AltBaro
 		alt_geom := rec.AltGeom
@@ -123,73 +148,78 @@ func main() {
 		sda := rec.Sda
 		alert := rec.Alert
 		spi := rec.Spi
-		mlat := rec.Mlat
-		tisb := rec.Tisb
 		messages := rec.Messages
 		seen := rec.Seen
 		rssi := rec.Rssi
 
-		var sb strings.Builder
-		sb.WriteString("{")
-		sb.WriteString("\"hex\": \"" + hex + "\", ")
-		sb.WriteString("\"ttype\": \"" + ttype + "\", ")
-		sb.WriteString("\"flight\": \"" + flight + "\", ")
-		sb.WriteString("\"alt_baro\": \"" + fmt.Sprintf("%d", alt_baro) + "\", ")
-		sb.WriteString("\"alt_geom\": \"" + fmt.Sprintf("%d", alt_geom) + "\", ")
-		sb.WriteString("\"gs\": \"" + fmt.Sprintf("%g", gs) + "\", ")
-		sb.WriteString("\"track\": \"" + fmt.Sprintf("%g", track) + "\", ")
-		sb.WriteString("\"baro_rate\": \"" + fmt.Sprintf("%d", baro_rate) + "\", ")
-		sb.WriteString("\"squawk\": \"" + squawk + "\", ")
-		sb.WriteString("\"emergency\": \"" + emergency + "\", ")
-		sb.WriteString("\"category\": \"" + category + "\", ")
-		sb.WriteString("\"lat\": \"" + fmt.Sprintf("%g", lat) + "\", ")
-		sb.WriteString("\"lon\": \"" + fmt.Sprintf("%g", lon) + "\", ")
-		sb.WriteString("\"nic\": \"" + fmt.Sprintf("%d", nic) + "\", ")
-		sb.WriteString("\"rc\": \"" + fmt.Sprintf("%d", rc) + "\", ")
-		sb.WriteString("\"seen_pos\": \"" + fmt.Sprintf("%g", seen_pos) + "\", ")
-		sb.WriteString("\"r_dst\": \"" + fmt.Sprintf("%g", r_dst) + "\", ")
-		sb.WriteString("\"r_dir\": \"" + fmt.Sprintf("%g", r_dir) + "\", ")
-		sb.WriteString("\"version\": \"" + fmt.Sprintf("%d", version) + "\", ")
-		sb.WriteString("\"nic_baro\": \"" + fmt.Sprintf("%d", nic_baro) + "\", ")
-		sb.WriteString("\"nac_p\": \"" + fmt.Sprintf("%d", nac_p) + "\", ")
-		sb.WriteString("\"nac_v\": \"" + fmt.Sprintf("%d", nac_v) + "\", ")
-		sb.WriteString("\"sil\": \"" + fmt.Sprintf("%d", sil) + "\", ")
-		sb.WriteString("\"sil_type\": \"" + sil_type + "\", ")
-		sb.WriteString("\"gva\": \"" + fmt.Sprintf("%d", gva) + "\", ")
-		sb.WriteString("\"sda\": \"" + fmt.Sprintf("%d", sda) + "\", ")
-		sb.WriteString("\"alert\": \"" + fmt.Sprintf("%d", alert) + "\", ")
-		sb.WriteString("\"spi\": \"" + fmt.Sprintf("%d", spi) + "\", ")
-		sb.WriteString("\"mlat\": \"" + fmt.Sprintf("%g", mlat) + "\", ")
-		sb.WriteString("\"tisb\": \"" + fmt.Sprintf("%g", tisb) + "\", ")
-		sb.WriteString("\"messages\": \"" + fmt.Sprintf("%d", messages) + "\", ")
-		sb.WriteString("\"seen\": \"" + fmt.Sprintf("%g", seen) + "\", ")
-		sb.WriteString("\"rssi\": \"" + fmt.Sprintf("%g", rssi) + "\"")
-		sb.WriteString("}")
+		sql := "CALL add_flight ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31);"
 
-		//--- Just hold this here
-		temp := sb.String()
-		jsonBody := []byte(temp)
-		apiPayload := bytes.NewReader(jsonBody)
-
-		//fmt.Println(temp)
-
-		//--- Send the request downrange to the API URL
-		req, err := http.NewRequest(http.MethodPost, fxApiUrl, apiPayload)
+		_, err := db.Exec(sql, hex_code, type_code, flight, alt_baro, alt_geom, gs, track, baro_rate, squawk, emergency, category, lat, lon, nic, rc, seen_pos, r_dst, r_dir, version, nic_baro, nac_p, nac_v, sil, sil_type, gva, sda, alert, spi, messages, seen, rssi)
 		if err != nil {
-			panic(err)
+			panic(err.Error())
 		}
-		req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-		req.Header.Set("x-api-key", fxApiKey)
 
-		client := &http.Client{}
-		response, error := client.Do(req)
-		//fmt.Println(response)
-		if error != nil {
-			panic(error)
-		}
-		defer response.Body.Close()
+		// var sb strings.Builder
+		// sb.WriteString("{")
+		// sb.WriteString("\"hex\": \"" + hex + "\", ")
+		// sb.WriteString("\"ttype\": \"" + ttype + "\", ")
+		// sb.WriteString("\"flight\": \"" + flight + "\", ")
+		// sb.WriteString("\"alt_baro\": \"" + fmt.Sprintf("%d", alt_baro) + "\", ")
+		// sb.WriteString("\"alt_geom\": \"" + fmt.Sprintf("%d", alt_geom) + "\", ")
+		// sb.WriteString("\"gs\": \"" + fmt.Sprintf("%g", gs) + "\", ")
+		// sb.WriteString("\"track\": \"" + fmt.Sprintf("%g", track) + "\", ")
+		// sb.WriteString("\"baro_rate\": \"" + fmt.Sprintf("%d", baro_rate) + "\", ")
+		// sb.WriteString("\"squawk\": \"" + squawk + "\", ")
+		// sb.WriteString("\"emergency\": \"" + emergency + "\", ")
+		// sb.WriteString("\"category\": \"" + category + "\", ")
+		// sb.WriteString("\"lat\": \"" + fmt.Sprintf("%g", lat) + "\", ")
+		// sb.WriteString("\"lon\": \"" + fmt.Sprintf("%g", lon) + "\", ")
+		// sb.WriteString("\"nic\": \"" + fmt.Sprintf("%d", nic) + "\", ")
+		// sb.WriteString("\"rc\": \"" + fmt.Sprintf("%d", rc) + "\", ")
+		// sb.WriteString("\"seen_pos\": \"" + fmt.Sprintf("%g", seen_pos) + "\", ")
+		// sb.WriteString("\"r_dst\": \"" + fmt.Sprintf("%g", r_dst) + "\", ")
+		// sb.WriteString("\"r_dir\": \"" + fmt.Sprintf("%g", r_dir) + "\", ")
+		// sb.WriteString("\"version\": \"" + fmt.Sprintf("%d", version) + "\", ")
+		// sb.WriteString("\"nic_baro\": \"" + fmt.Sprintf("%d", nic_baro) + "\", ")
+		// sb.WriteString("\"nac_p\": \"" + fmt.Sprintf("%d", nac_p) + "\", ")
+		// sb.WriteString("\"nac_v\": \"" + fmt.Sprintf("%d", nac_v) + "\", ")
+		// sb.WriteString("\"sil\": \"" + fmt.Sprintf("%d", sil) + "\", ")
+		// sb.WriteString("\"sil_type\": \"" + sil_type + "\", ")
+		// sb.WriteString("\"gva\": \"" + fmt.Sprintf("%d", gva) + "\", ")
+		// sb.WriteString("\"sda\": \"" + fmt.Sprintf("%d", sda) + "\", ")
+		// sb.WriteString("\"alert\": \"" + fmt.Sprintf("%d", alert) + "\", ")
+		// sb.WriteString("\"spi\": \"" + fmt.Sprintf("%d", spi) + "\", ")
+		// sb.WriteString("\"mlat\": \"" + fmt.Sprintf("%g", mlat) + "\", ")
+		// sb.WriteString("\"tisb\": \"" + fmt.Sprintf("%g", tisb) + "\", ")
+		// sb.WriteString("\"messages\": \"" + fmt.Sprintf("%d", messages) + "\", ")
+		// sb.WriteString("\"seen\": \"" + fmt.Sprintf("%g", seen) + "\", ")
+		// sb.WriteString("\"rssi\": \"" + fmt.Sprintf("%g", rssi) + "\"")
+		// sb.WriteString("}")
 
-		//--- Logging
-		fmt.Println("\"hex\": \""+hex+"\" -  API response:", response.Status)
+		// //--- Just hold this here
+		// temp := sb.String()
+		// jsonBody := []byte(temp)
+		// apiPayload := bytes.NewReader(jsonBody)
+
+		// fmt.Println(temp)
+
+		// //--- Send the request downrange to the API URL
+		// req, err := http.NewRequest(http.MethodPost, fxApiUrl, apiPayload)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+		// req.Header.Set("x-api-key", fxApiKey)
+
+		// client := &http.Client{}
+		// response, error := client.Do(req)
+		// //fmt.Println(response)
+		// if error != nil {
+		// 	panic(error)
+		// }
+		// defer response.Body.Close()
+
+		// //--- Logging
+		// fmt.Println("\"hex\": \""+hex+"\" -  API response:", response.Status)
 	}
 }
