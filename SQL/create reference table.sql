44523347f4bd4dc62d5cb0e9f552ef650557f710
Mike Tickle <mtickle@gmail.com>
2023-09-18 03:19:06 -0400

    updates

--- SQL/create reference table.sql
+++ SQL/create reference table.sql
@@ -1,15 +1,37 @@
--- Table: public.reference
+-- Table: public.adsb_reference
 
--- DROP TABLE IF EXISTS public.reference;
 
-CREATE TABLE IF NOT EXISTS public.reference
+
+-- DROP TABLE IF EXISTS public.adsb_reference;
+
+CREATE TABLE IF NOT EXISTS public.adsb_reference
 (
-    "CODE" text COLLATE pg_catalog."default",
-    "MFR" text COLLATE pg_catalog."default",
-    "MODEL" text COLLATE pg_catalog."default"
+    hex_code character varying COLLATE pg_catalog."default",
+	reg_code character varying COLLATE pg_catalog."default",
+    manufacturer_icao character varying COLLATE pg_catalog."default",
+    manufacturer_name character varying COLLATE pg_catalog."default",
+    type_code character varying COLLATE pg_catalog."default",
+    model_code character varying COLLATE pg_catalog."default",
+    serial_number character varying COLLATE pg_catalog."default",
+    tbd character varying COLLATE pg_catalog."default",
+    icao_aircraft_type character varying COLLATE pg_catalog."default",
+    operator_name character varying COLLATE pg_catalog."default",
+    operator_icao character varying COLLATE pg_catalog."default",
+    operator_callsign character varying COLLATE pg_catalog."default",
+    tbd2 character varying COLLATE pg_catalog."default",
+    owner_name character varying COLLATE pg_catalog."default",
+    tbd3 character varying COLLATE pg_catalog."default"
+    
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.reference
+ALTER TABLE IF EXISTS public.adsb_reference
     OWNER to pi;
+
+
+
+    COPY adsb_reference(hex_code, reg_code, manufacturer_icao, manufacturer_name, type_code, model_code, serial_number, tbd, icao_aircraft_type, operator_name, operator_icao, operator_callsign, tbd2, owner_name, tbd3)
+FROM '/tmp/db2.csv'
+DELIMITER ','
+CSV HEADER;
--- SQL/mv_flights.sql
+++ SQL/mv_flights.sql
@@ -1,20 +1,30 @@
--- View: public.mv_flight
+-- View: public.mv_all_flights
 
--- DROP MATERIALIZED VIEW IF EXISTS public.mv_flight;
+-- DROP MATERIALIZED VIEW IF EXISTS public.mv_all_flights;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS public.mv_all_flight
+CREATE MATERIALIZED VIEW IF NOT EXISTS public.mv_all_flights
 TABLESPACE pg_default
 AS
- SELECT flight.hex_code,
+  SELECT flight.hex_code,
     flight.flight_code,
     flight.flight_time,
-    master.name,
+    flight.type_code,
+    flight.category,
+    flight.squawk,
+    adsb_reference.operator_icao,
+	adsb_reference.owner_name,
+	master.name,
+	adsb_reference.manufacturer_name,
     reference.mfr,
+	adsb_reference.model_code,
     reference.model
+	
    FROM flight
+   		join adsb_reference on lower(flight.hex_code::text) = adsb_reference.hex_code
      JOIN master ON flight.hex_code::text = master.mode_s_code_hex
      JOIN reference ON master.mfr_mdl_code = reference.code
+	 ORDER by flight_time desc
 WITH DATA;
 
-ALTER TABLE IF EXISTS public.mv_flight
+ALTER TABLE IF EXISTS public.mv_all_flights
     OWNER TO pi;
